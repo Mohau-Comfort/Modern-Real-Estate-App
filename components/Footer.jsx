b046788a2a4ecc126f966a604a680933bdfd269f
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-09-24 09:29:44 +0200

    updated copyright names

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,10 @@ import { Box } from '@chakra-ui/layout';
 
 const Footer = () => (
   <Box textAlign='center' p='5' color='gray.600' borderTop='1px' borderColor='gray.100'>
-    © 2021 Realtor, Inc.
+           <div className="copyright">
+          <p className="p-text">Copyright © {new Date().getFullYear()} Mohau Real Estate</p>
+          <p className="p-text">All rights reserved</p>
+        </div>
   </Box>
 );
 
